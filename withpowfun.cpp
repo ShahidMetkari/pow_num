c5d51168c59a8c31a703329ecd302187542b59b5
ShahidMetkari <148204776+ShahidMetkari@users.noreply.github.com>
2023-10-17 03:12:53 -0700

    Update withpowfun.cpp

--- withpowfun.cpp
+++ withpowfun.cpp
@@ -1,18 +1,49 @@
-#include <iostream>
-#include <cmath>
-
+#include<iostream>
 using namespace std;
 
-int main() 
+double Power(double n,int p=2)
 {
-    float base, exponent, result;
+    double ans=1;
+    if(p==0)
+       return ans;
+    else
+ {
+    for(int i=1;i<=p;i++)
+    {ans=ans*n;}
+    return ans;
+ }
+}
 
-    cout << "Enter base and exponent respectively:  ";
-    cin >> base >> exponent;
 
-    result = pow(base, exponent);
+double Recurse(double n,int p=2)
+{
 
-    cout << base << "^" << exponent << " = " << result;
-    
-    return 0;
+    if(p==0)
+      {return 1;}
+
+    if(p==1)
+    {return n;}
+
+    else
+    {return (n*Recurse(n,p-1));}
+
+}
+
+
+int main()
+
+{
+double n;
+int p;
+
+    cout<<"Enter the base value (power is default set to 2) :";
+    cin>>n;
+    cout<<"\nPower of number is : "<<Power(n);
+    cout<<"\nEnter the base value and power :";
+    cin>>n>>p;
+    cout<<"\nPower of number is : "<<Power(n,p);
+    cout<<"\nEnter the base value and power :";
+    cin>>n>>p;
+    cout<<"\nUsing Recursion, Power of number is : "<<Recurse(n,p);
+return 0;
 }
